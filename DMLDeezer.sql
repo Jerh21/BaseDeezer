b53811a66b85d06e5ac5380bf8bd8de4020edca1
Jerh21 <hamjosue27@gmail.com>
2024-12-09 13:38:08 -0600

    actualizados identidades

--- DMLDeezer.sql
+++ DMLDeezer.sql
@@ -1,10 +1,9 @@
 REM INSERTING into C##DEEZER.TBL_IDENTIDADES
 SET DEFINE OFF;
-INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (1, 'Hombre');
-INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (2, 'Mujer');
+INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (1, 'Masculino');
+INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (2, 'Femenino');
 INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (3, 'No Binario');
-INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (4, 'Terreneitor');
-INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (5, 'Otro');
+INSERT INTO C##DEEZER.TBL_IDENTIDADES (codigo_identidad, identidad) VALUES (4, 'Otro');
 
 
 REM INSERTING into C##DEEZER.TBL_ARTISTAS
